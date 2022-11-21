d58a04fcb40a0f10b6d6473691907253e57438bd
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-21 13:40:48 -0800

    Task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@ AirBnB_clone_v3 API
 
 from os import getenv
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
