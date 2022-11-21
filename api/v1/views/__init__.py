6d0bc67c4def9c68b76f8a9e091fdcdcb8fbe7c5
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-21 11:53:24 -0800

    bug fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ AirBnB_clone_v3 API
 """
 
 
-from api.v1.views.index import *
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
